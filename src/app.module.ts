808421a16d720d9fcd3ba951cc889109ba473326
akhzamov <abdulmalikakhzamov1@gmail.com>
2024-06-13 09:39:35 +0500

    Add Test controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AmocrmModule } from './amocrm/amocrm.module';
+import { TestController } from './test/test.controller';
 
 @Module({
   imports: [AmocrmModule],
-  controllers: [AppController],
+  controllers: [AppController, TestController],
   providers: [AppService],
 })
 export class AppModule {}
