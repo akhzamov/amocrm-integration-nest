e9676c60ccea77f48cb900d290d110cdcfeed226
akhzamov <abdulmalikakhzamov1@gmail.com>
2024-06-13 10:13:58 +0500

    edited production CORS domen

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: process.env.NODE_ENV === 'production'
-      ? ['https://amocrm-leads.vercel.app/']
+      ? ['https://amocrm-leads.vercel.app']
       : ['http://localhost:5173'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept',
